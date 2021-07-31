8570ecc7cacf22eafcca9f6193213229531b42bf
Justin Turner <jmturner638@gmail.com>
2021-07-31 00:23:30 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,17 @@
 def main():
     #write your code below this line
+    print("Greatest:", greatest(2,7,9))
+
+def greatest(number1, number2, number3):
+    greatest = number1
+
+    if number2 > greatest:
+        greatest = number2
+    
+    if number3 > greatest:
+        greatest = number3
+
+    return greatest
 
 if __name__ == '__main__':
     main()
